20ba6562638072286494689b18bc3750555b67d8
Lizette Ayala <ayall291@newschool.edu>
2022-10-08 10:47:39 -0400

    Add files via upload

--- global.js
+++ global.js
@@ -1,6 +1,9 @@
+
+
 /*** Document Load Test ***/
 $(document).ready(function() {
   console.log("document loaded");
+  window.n = 0
 });
 
 /***  Nav Bar Desktop to Mobile view Responsiveness ***/
@@ -48,7 +51,14 @@ const isMobile = window.matchMedia("only screen and (min-device-width: 320px) an
 $(window).on("load", function() {
   console.log("window loaded");
 
-  if (isDesktop) {
+  // var opening_animation = false
+  // if (
+  //   isDesktop
+  //   && opening_animation
+  // ) 
+
+
+  {
 
     $( "#all-content" ).delay( 6000 ).animate({ "marginTop":"-35px" }, 2000 );
     $( "#top-part" ).delay( 6000 ).animate({ height: "15%" }, 2000 );
@@ -59,6 +69,7 @@ $(window).on("load", function() {
     $( "#opening-poem" ).delay( 2000 ).animate({ "opacity":"1" }, 3000 );
     $( "#opening-poem" ).delay( 8500 ).fadeIn( 2000 );
     $( "#first-opening-p" ).delay( 9000 ).toggleClass('scroll', 3000 );
+
   }
 
   if (isMobile) {}
@@ -76,38 +87,45 @@ $(window).on("load", function() {
 
     if (isDesktop) {
 
-      window.n = this.id.slice(-1)
-      if (n == 1) {
-        $("#first-opening-p").delay(2000).animate({height: "1.5vh"}, 3000);
-        $("#all-content").delay(2500).animate({"marginTop": "5px"}, 3000);
-        $("#all-content").delay(2500).animate({height: "93%"}, 3000);
-        $("#title-top-01").delay(2500).addClass("remove", 3000);
-        $("#top-part").delay(2500).animate({height: "70%"}, 3000);
-        $("#bottom-part").delay(2500).animate({height: "20%"}, 3000);
-        $("#main-opacity").delay(1000).addClass("full-opacity", 3000);
-        $("#main-opacity").delay(1000).animate({height: "15%"}, 3000);
-        $("#first-opening-p").delay(6000).toggleClass('scroll', 3000);
-        $("#experience").delay(3000).animate({height: "80%"}, 3000);
-        $("#all-isolated-title").delay(3000).animate({height: "15%"}, 3000);
-        $("#second-brick").delay(3000).animate({width: "0vw"}, 3000);
-        $("#second-brick").delay(0).fadeOut(3000);
-        $("#first-brick").delay(3000).animate({width: "101vw"}, 3000);
-        $("#fourth-brick").delay(1000).addClass("bg-change", 3000);
-        $("#first-brick").delay(3000).addClass("experience-bg", 3000);
-      }
+      var target = parseInt(this.id.slice(-1))
+      if (target == n + 1 || n == 5 && target == 1) {
+        if (n == '0') {
+          window.n = target
+          $("#first-opening-p").delay(2000).animate({height: "1.5vh"}, 3000);
+          $("#all-content").delay(2500).animate({"marginTop": "5px"}, 3000);
+          $("#all-content").delay(2500).animate({height: "93%"}, 3000);
+          $("#title-top-01").delay(2500).addClass("remove", 3000);
+          $("#top-part").delay(2500).animate({height: "70%"}, 3000);
+          $("#bottom-part").delay(2500).animate({height: "20%"}, 3000);
+          $("#main-opacity").delay(1000).addClass("full-opacity", 3000);
+          $("#main-opacity").delay(1000).animate({height: "15%"}, 3000);
+          $("#first-opening-p").delay(6000).toggleClass('scroll', 3000);
+          $("#experience").delay(3000).animate({height: "80%"}, 3000);
+          $("#all-isolated-title").delay(3000).animate({height: "15%"}, 3000);
+          $("#second-brick").delay(3000).animate({width: "0vw"}, 3000);
+          $("#second-brick").delay(0).fadeOut(3000);
+          $("#first-brick").delay(3000).animate({width: "101vw"}, 3000);
+          $("#fourth-brick").delay(1000).addClass("bg-change", 3000);
+          $("#first-brick").delay(3000).addClass("experience-bg", 3000);
+        }
 
-      else {
-        $('[id^="isolated"]').fadeOut(2000);
-        $(`[id^="isolated"][n=${n}]`).delay(1000).fadeIn(2000);
-        $(`[id^="isolated"]:not([n=${n}])`).delay(1000).animate({height: "0%"}, 2000);
-        $('.experience-poem').fadeOut(2000);
-        $(`#ex-0${n}`).delay(1500).fadeIn(2000);
+        else {
+          window.n = target
+          $('[id^="isolated"]').fadeOut(2000);
+          $(`[id^="isolated"][n=${n}]`).delay(1000).fadeIn(2000);
+          $(`[id^="isolated"]:not([n=${n}])`).delay(1000).animate({height: "0%"}, 2000);
+          $('.experience-poem').fadeOut(2000);
+          $(`#ex-0${n}`).delay(1500).fadeIn(2000);
+        }
       }
 
     }
     else if (isMobile) {}
-    return false;
+    return false
 
   })
 
-});
+  //////////////////
+  console.clear()
+
+})
